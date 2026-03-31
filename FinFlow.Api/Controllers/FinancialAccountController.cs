c0c0ff160854d9d05596c0b9ce8df4031f77f45b
Filipe Fontes <filipe10fontes@live.com.pt>
2026-03-31 09:07:16 +0000

    Created first endpoints and added DbContext

---
+++ FinFlow.Api/Controllers/FinancialAccountController.cs
@@ -0,0 +1,57 @@
+﻿using FinFlow.Application.Interfaces;
+using FinFlow.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinFlow.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FinancialAccountController : ControllerBase
+    {
+        private readonly IFinancialAccountService _service;
+
+        public FinancialAccountController(IFinancialAccountService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet ("myFinancialAccounts")]
+        public async Task<IActionResult> GetAllFinancialAccount()
+        {
+            var accounts = await _service.GetAllFinancialAccountAsync();
+            return Ok(accounts);
+        }
+
+        [HttpGet("getFinancialAccountById/{id}")]
+        public async Task<IActionResult> GetFinancialAccountById(Guid id)
+        {
+            var account = await _service.GetFinancialAccountByIdAsync(id);
+            if(account == null) { return NotFound(); }
+            return Ok(account);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFinancialAccountAsync([FromBody] FinancialAccount account)
+        {
+            var createdAccount = await _service.CreateFinancialAccountAsync(account);
+            return CreatedAtAction(nameof(GetFinancialAccountById), new { id = createdAccount.Id }, createdAccount);
+        }
+
+        [HttpPut("updateFinancialAccout/{id}")]
+        public async Task<IActionResult> UpdateFinancialAccountAsync(Guid id, [FromBody] FinancialAccount account)
+        {
+            var updatedAccount = await _service.UpdateAsync(id, account);
+            if (updatedAccount == null) { return NotFound(); }
+            return Ok(updatedAccount);
+        }
+
+        [HttpDelete("deleteFinancialAccount/{id}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            var deleted = await _service.DeleteAsync(id);
+            if (!deleted) return NotFound();
+            return NoContent();
+        }
+
+    }
+}
---
+++ FinFlow.Infrastructure/Repositories/FinancialAccountRepository.cs
@@ -0,0 +1,49 @@
+﻿using FinFlow.Application.Interfaces;
+using FinFlow.Domain.Models;
+using FinFlow.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinFlow.Infrastructure.Repositories
+{
+    public class FinancialAccountRepository : IFinancialAccountRepository
+    {
+        private readonly FinFlowDbContext _context;
+
+        public FinancialAccountRepository(FinFlowDbContext context)
+        { 
+            _context = context;
+        }
+
+        public async Task<IEnumerable<FinancialAccount>> GetAllFinancialAccountAsync()
+        {
+            return await _context.FinancialAccounts.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<FinancialAccount?> GetFinancialAccountByIdAsync(Guid id)
+        {
+            return await _context.FinancialAccounts.FindAsync(id);
+        }
+
+        public async Task AddFinancialAccountAsync(FinancialAccount account)
+        {
+            await _context.FinancialAccounts.AddAsync(account);
+        }
+
+        public Task UpdateAsync(FinancialAccount account)
+        {
+            _context.FinancialAccounts.Update(account);
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteAsync(FinancialAccount account)
+        {
+           _context.FinancialAccounts.Remove(account);
+            return Task.CompletedTask;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
