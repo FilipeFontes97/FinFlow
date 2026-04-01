d49c22afb0e84a4a782ad7ea5036d60212f64a81
Filipe Fontes <filipe10fontes@live.com.pt>
2026-04-01 09:40:26 +0000

    Configs

--- FinFlow.Api/Program.cs
+++ FinFlow.Api/Program.cs
@@ -11,47 +11,41 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReact", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins(
+                "https://localhost:5247"
+            )
             .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials();
+            .AllowAnyMethod();
     });
 });
 
-// Register DbContext
 builder.Services.AddDbContext<FinFlowDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
 
-// Add controllers, swagger, etc.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-// Add Services
-builder.Services.AddScoped<IFinancialAccountService, FinancialAccountService>();
 
-// Add Repositories
+builder.Services.AddScoped<IFinancialAccountService, FinancialAccountService>();
 builder.Services.AddScoped<IFinancialAccountRepository, FinancialAccountRepository>();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    // Generate Swagger/OpenAPI JSON at /swagger/v1/swagger.json
     app.UseSwagger();
-
     app.UseSwaggerUI(c =>
     {
-        // Default generated JSON endpoint
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
         c.RoutePrefix = "swagger";
     });
 }
 
 app.UseHttpsRedirection();
 app.UseCors("AllowReact");
-app.UseAuthentication();
 app.UseAuthorization();
-// Serve uploaded files from wwwroot (e.g. /uploads/filename)
-app.UseStaticFiles();
 app.MapControllers();
 app.Run();
